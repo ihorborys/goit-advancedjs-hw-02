868169852f2dc1534caa935612de069a5f9af15d
ihorborys <ihorborys@gmail.com>
2025-03-10 23:01:39 +0200

    Edit script path

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: "./", // Виправляє шляхи до стилів у продакшені
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
