802df1240c6a69ecf255000615e340e3d8223364
ihorborys <ihorborys@gmail.com>
2025-03-10 21:46:35 +0200

    Add task 1, task 2

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,59 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
+const form = document.querySelector('.form')
+const delayInput = document.querySelector('.delay-input')
+
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+
+  const delay = delayInput.value
+  const stateValue = document.querySelector('input[name="state"]:checked')
+
+
+  const fulfilledMessage = (message) => {
+    iziToast.success({
+      title: `✅ Fulfilled promise in ${delay} ms`,
+      message: message,
+      position: 'topRight',
+      color: 'green',
+      icon: "",
+    });
+  };
+
+  const rejectedMessage = (message) => {
+    iziToast.error({
+      title: `❌ Rejected promise in ${delay} ms`,
+      message: message,
+      position: 'topRight',
+      color: 'red',
+      icon: ""
+    });
+  };
+
+
+  const promiseGenerator = new Promise((resolve, reject) =>  {
+
+    setTimeout(() => {
+
+      if (stateValue.value === 'fulfilled') {
+        resolve();
+
+      } else {
+        reject();
+      }
+
+    }, delay);
+
+  });
+
+  promiseGenerator
+    .then(() => fulfilledMessage(''))
+    .catch(() => rejectedMessage(''));
+
+  form.reset()
+
+})
